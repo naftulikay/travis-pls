199559372f5a924772f166f0d23f31009e5d5829
Naftuli Kay <me@naftuli.wtf>
2017-03-10 13:07:17 -0800

    Bump release to 1.0.2.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "travispls",
-    version = "1.0.1",
+    version = "1.0.2",
     packages = find_packages('src'),
     package_dir = { '': 'src'},
     author = "Naftuli Kay",
